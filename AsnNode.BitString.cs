43f628ee70ad6400379ba25bf30830a306e48007
Kevin Jones <kevin@vcsjones.com>
2023-11-15 10:58:01 -0500

    Fix bitstring to decode like octet string

--- AsnNode.BitString.cs
+++ AsnNode.BitString.cs
@@ -35,16 +35,7 @@ public sealed class PrimitiveBitStringAsnNode : AsnNode {
 
         // Slice off the unused bit count byte.
         AsnWalker walker = new(Context with { Synthetic = true }, Contents[1..]);
-
-        try {
-            // We want to up-front validate all of the contents so that we can back-out
-            // if it turns out we can't walk it.
-            return walker.Walk().ToArray();
-        }
-        catch {
-            return Array.Empty<AsnNode>();
-        }
-
+        return DecodeChildren(walker);
     }
 
     public override string Display {
