eb77cdd5d39ceeae351c04793d6ba86eabbd1a95
Kevin Jones <kevin@vcsjones.com>
2024-05-10 22:52:44 -0400

    GeneralizedTime show the raw encoded value

--- AsnNode.GeneralizedTime.cs
+++ AsnNode.GeneralizedTime.cs
@@ -14,9 +14,21 @@ public partial class AsnNode {
 
 public sealed class GeneralizedTimeAsnNode : AsnNode {
     private readonly DateTimeOffset _value;
+    private readonly string _stringValue;
 
     public GeneralizedTimeAsnNode(Asn1Tag tag, AsnWalkContext context, AsnReader reader) : base(tag, context, reader) {
+        ReadOnlyMemory<byte> bytes = reader.PeekContentBytes();
         _value = reader.ReadGeneralizedTime(tag);
+
+        // If reading as a generalized time worked, then we should be able to decode the contents as an ASCII string.
+        _stringValue = System.Text.Encoding.ASCII.GetString(bytes.Span);
+    }
+
+    public override List<(string Name, string Value)> GetAdorningAttributes()
+    {
+        var attributes = base.GetAdorningAttributes();
+        attributes.Add(("Value", _stringValue));
+        return attributes;
     }
 
     public override string Display => _value.ToString();
